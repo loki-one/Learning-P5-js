f102d6bea4027bd8dccfb93f6598bb387be540d1
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2017-10-07 16:36:28 +0200

    Add sun and grass

--- Project 3 Understanding Color/sketch.js
+++ Project 3 Understanding Color/sketch.js
@@ -6,13 +6,19 @@ function setup() {
 function draw() {
   /*Fourth argument in fill is transparency*/
   noStroke(); //To omit outlines or borders
+  //Sun
+  fill(255,255,0);
+  ellipse(100,50,50,50);
   //Roof
   fill(0,0,255);
   triangle(225,150,200,200,250,200);
-  //body
+  //Body
   fill(255,0,0);
   rect(200,200,50,50);
-  //drawing a door
+  //Door
   fill(0,255,0);
   rect(220,220,15,30);
+  //Grass
+  fill(0,80,0);
+  rect(0,250,600,350);
 }
